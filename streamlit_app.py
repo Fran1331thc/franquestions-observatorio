47a80a9c3d7312603781235de4906fb23d04707e
Fran1331thc <89511750+Fran1331thc@users.noreply.github.com>
2026-07-28 15:11:11 -0600

    Disable watcher before public app import

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,7 @@ import os
 
 os.environ.setdefault("FQ_ENV", "production")
 os.environ.setdefault("FQ_API_URL", "http://127.0.0.1:9")
+os.environ.setdefault("STREAMLIT_SERVER_FILE_WATCHER_TYPE", "none")
+os.environ.setdefault("STREAMLIT_SERVER_RUN_ON_SAVE", "false")
 
 from fq_observatorio.dashboard import *  # noqa: F401,F403,E402
